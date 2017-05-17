2a9c3b4eb291b094dbe2f55463cfb2e547ecc074
jigglor <andrewtsuji@gmail.com>
2017-05-17 16:32:20 -0700

    added project list state and cards for projects

--- app/app.js
+++ app/app.js
@@ -8,8 +8,88 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
             .state('main', {
                 url: '/',
                 templateUrl: 'app/views/home.html'
+            })
+            .state('projects', {
+                url: '/projects',
+                templateUrl: 'app/views/projectList.html',
+                controller: 'ProjectListCtrl'
             });
 
         $locationProvider.html5Mode(true);
     }
 ]);
+
+app.controller('ProjectListCtrl', ['$scope', 'Projects', function($scope, Projects) {
+    $scope.projects = Projects;
+}]);
+
+app.factory('Projects', function() {
+    return {
+        '1': {
+            title: 'Cat Fight!',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'A Pokemon-style battle game designed in javascipt.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        },
+        '2': {
+            title: 'Brewzaar',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'A web app designed to help people trade the beer they have for the beer they want.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        },
+        '3': {
+            title: 'Project 3',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        },
+        '4': {
+            title: 'Project 4',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        },
+        '5': {
+            title: 'Project 5',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        },
+        '6': {
+            title: 'Project 6',
+            cover: '/img/folder.png',
+            github: '#!',
+            img: [],
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
+            link: {
+                title: 'Live Site',
+                url: '#!'
+            }
+        }
+    };
+});
