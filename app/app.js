482fa714487be15d70e1b44988a8e5b01205d86b
jigglor <andrewtsuji@gmail.com>
2017-06-21 15:36:52 -0700

    changed color scheme.  added tech used to each project, and updated descriptions

--- app/app.js
+++ app/app.js
@@ -34,16 +34,17 @@ app.controller('ProjectShowCtrl', ['$scope', 'Projects', '$stateParams',
 
 app.factory('Projects', function() {
   return {
-    '1': {
-      title: 'Cat Fight!',
-      cover: '/img/cat-fight.png',
-      github: 'https://github.com/jigglor/u1-project-game',
-      img: ['/img/cat-fight1.png',
-            '/img/cat-fight2.png'],
-      desc: 'A JavaScript, Pokemon-style battle game.  2 players pick 2 cats each and battle to the death!',
-      longDesc: 'This was this very first website I ever designed on my own.  I played Pokemon a lot as a kid and I had a ton of fun trying to code a battle system that was reminiscent of those games.  It\'s certainly not as robust or polished, but I hope you have fun playing it nonetheless.',
-      link: 'https://jigglor.github.io/u1-project-game/'
-    },
+    // '1': {
+    //   title: 'Cat Fight!',
+    //   cover: '/img/cat-fight.png',
+    //   github: 'https://github.com/jigglor/u1-project-game',
+    //   img: ['/img/cat-fight1.png',
+    //         '/img/cat-fight2.png'],
+    //   desc: 'A JavaScript, Pokemon-style battle game.  2 players pick 2 cats each and battle to the death!',
+    //   longDesc: 'This was this very first website I ever designed on my own.  I played Pokemon a lot as a kid and I had a ton of fun trying to code a battle system that was reminiscent of those games.  It\'s certainly not as robust or polished, but I hope you have fun playing it nonetheless.',
+    // tech: [],
+    //   link: 'https://jigglor.github.io/u1-project-game/'
+    // },
     '2': {
       title: 'Brewzaar',
       cover: '/img/brewzaar.png',
@@ -52,6 +53,12 @@ app.factory('Projects', function() {
             '/img/brewzaar2.png'],
       desc: 'A web app designed to help people trade the beer they have for the beer they want.',
       longDesc: 'Having worked in the beer industry for a couple years, I\'ve been fairly blessed when it comes to getting my hands on rare or out-of-state beers.  Not everyone can be so lucky, however, so I wanted to design a website that would connect beer enthusiasts and make it easy for them to track down the beers they desire most.',
+      tech: ['BreweryDB API',
+             'HTML/EJS Layouts',
+             'CSS/Materialize',
+             'JavaScript/jQuery',
+             'Node.js/Express',
+             'MongoDB/Mongoose'],
       link: 'https://brewzaar.herokuapp.com/home'
     },
     '3': {
@@ -63,7 +70,15 @@ app.factory('Projects', function() {
             '/img/doggleganger3.png',
             '/img/doggleganger4.png'],
       desc: 'Ready to adopt a pet?  Search for a pet and then compare your face with the animals to see how good of a match you are.',
-      longDesc: 'I love animals and it upsets me to hear about so many going unadopted.  Designing a web app that encourages people to rescue animals from shelters was both fun and rewarding.  Search for a pet you might like to adopt and then compare their photo to a photo of yourself.  Find your doggleganger today!',
+      longDesc: 'I love animals and it upsets me to hear about so many going unadopted.  Designing a web app that encourages people to rescue animals from shelters was both fun and rewarding.  I worked on this project with 2 other developers and was mainly responsible for building our backend, utilizing the Petfinder API.  I also built our Profile page and coded the media queries for the entire app.',
+      tech: ['Petfinder API',
+             'GraphicsMagick Node Module',
+             'HTML',
+             'CSS',
+             'JavaScript',
+             'Angular',
+             'Node.js/Express',
+             'MongoDB/Mongoose'],
       link: 'http://doggleganger.herokuapp.com/'
     },
     '4': {
@@ -76,6 +91,11 @@ app.factory('Projects', function() {
             '/img/chronotron4.png'],
       desc: 'Have some free time?  Chronotron can help you waste it.  Give it a length of time you\'d like to waste and choose a category.  Chronotron will provide you with content to fill that void.',
       longDesc: 'Chronotron utilizes 5 different APIs:  Youtube, Spotify, OpenTrivia, Trails and OpenWeather.  I worked on this in with a group of 4 other developers and 1 UX/UI consultant.  I was responsible for utilizing the OpenTrivia API to create a trivia game we could use within the app.  I built the backend in Ruby on Rails and used JavaScript and jQuery to create the trivia game.',
+      tech: ['Youtube, Spotify, Trails, OpenTrivia, and OpenWeather APIs',
+             'HTML',
+             'CSS',
+             'JavaScript/jQuery',
+             'Ruby on Rails'],
       link: 'https://chronotron.herokuapp.com/'
     }
     // '5': {
@@ -85,6 +105,7 @@ app.factory('Projects', function() {
     //     img: [],
     //     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
     // longDesc: '',
+    // tech: [],
     //     link: '#!'
     // },
     // '6': {
@@ -94,6 +115,7 @@ app.factory('Projects', function() {
     //     img: [],
     //     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci officia beatae.',
     // longDesc: '',
+    // tech: [],
     //     link: '#!'
     // }
   };
